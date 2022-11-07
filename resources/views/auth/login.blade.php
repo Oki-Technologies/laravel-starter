94a59741ec9c4fdef016e62f52a1051bd2299f83
Solomon Ochepa <solomonochepa@gmail.com>
2022-11-07 12:23:44 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,17 +12,21 @@
             </div>
         @endif
 
-        <form method="POST" action="{{ route('login') }}">
+        <form livewire="wire:submit.prevent='login'" method="POST">
             @csrf
 
+            {{-- Username --}}
             <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-label for="username" value="{{ __('Username') }}" />
+                <x-jet-input id="username" class="block mt-1 w-full @error('username') is-invalid @enderror"
+                    type="text" name="username" :value="old('username')" required autofocus wire:model="username" />
             </div>
 
+            {{-- Password --}}
             <div class="mt-4">
                 <x-jet-label for="password" value="{{ __('Password') }}" />
-                <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
+                <x-jet-input id="password" class="block mt-1 w-full @error('password') is-invalid @enderror"
+                    type="password" name="password" required autocomplete="current-password" wire:model="password" />
             </div>
 
             <div class="block mt-4">
@@ -34,7 +38,8 @@
 
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
+                    <a class="underline text-sm text-gray-600 hover:text-gray-900"
+                        href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
