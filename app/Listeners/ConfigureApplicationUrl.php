c411ab9fe7be8eaf858591dbc8a743bbd97860db
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:13:23 +0100

    Create ConfigureApplicationUrl.php

---
+++ app/Listeners/ConfigureApplicationUrl.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\URL;
+use Tenancy\Affects\URLs\Events\ConfigureURL;
+use Illuminate\Support\Str;
+
+class ConfigureApplicationUrl
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle(ConfigureURL $event)
+    {
+        if ($tenant = $event->event->tenant) {
+            // $event->changeRoot($tenant->slug . '.test');
+            $event->changeRoot(url($tenant->path));
+        }
+    }
+}
