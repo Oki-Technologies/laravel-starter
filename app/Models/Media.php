4b8eceba885ccc6857738d12dfafaef2ba0d5371
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:15:04 +0100

    Update Media.php

--- app/Models/Media.php
+++ app/Models/Media.php
@@ -6,9 +6,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Plank\Mediable\Media as PlankMedia;
+use Tenancy\Affects\Connections\Support\Traits\OnTenant;
 
 class Media extends PlankMedia
 {
     use HasFactory;
     use HasUuids;
+    use OnTenant;
 }
