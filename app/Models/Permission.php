b28d55c77157228a00cf29cd503817beb7e56365
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:15:10 +0100

    Update Permission.php

--- app/Models/Permission.php
+++ app/Models/Permission.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Spatie\Permission\Models\Permission as SpatiePermission;
+use Tenancy\Affects\Connections\Support\Traits\OnTenant;
 
 class Permission extends SpatiePermission
 {
     use HasFactory;
     use HasUuids;
+    use OnTenant;
 }
