bdda06cdade5c2468de6d8b04fc13e4d8098f936
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:17:15 +0100

    Create dashboard.blade.php

---
+++ resources/views/tenant/dashboard.blade.php
@@ -0,0 +1,13 @@
+<x-tenant-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ $business->name }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            ...rooms, visitors, residents, apartments,...
+        </div>
+    </div>
+</x-tenant-layout>
