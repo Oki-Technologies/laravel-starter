7de3febf88626f53ae44e842c4625d60e7de140e
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:17:05 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\BusinessController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -25,4 +26,8 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+
+    Route::resource('business', BusinessController::class)->except(['show'])->names('office.business');
+    // Tenant Dashboard
+    Route::get('{business}', [BusinessController::class, 'show'])->name('office.business.show');
 });
