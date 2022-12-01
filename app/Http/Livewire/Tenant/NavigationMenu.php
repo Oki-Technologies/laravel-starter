4d4c15307c257ff514fcea20b0b47b0fe63b6aa8
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:13:18 +0100

    Create NavigationMenu.php

---
+++ app/Http/Livewire/Tenant/NavigationMenu.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Tenant;
+
+use Livewire\Component;
+
+class NavigationMenu extends Component
+{
+    public function render()
+    {
+        return view('livewire.tenant.navigation-menu');
+    }
+}
