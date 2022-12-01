29deee3a0669f4921442b0440d59be9bdc275515
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:16:55 +0100

    Tenant migrations

---
+++ database/migrations/2022_11_30_180848_create_businesses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('businesses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->index();
+            $table->string('slug');
+            $table->string('hostname')->index();
+            $table->string('path')->nullable();
+            $table->timestamps();
+
+            $table->unique(['hostname', 'path'], 'hostname');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('businesses');
+    }
+};
