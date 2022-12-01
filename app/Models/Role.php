1c0ba40f2d7a6c6413c2fb803904a333cebe9dfa
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-01 13:15:14 +0100

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -5,9 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Spatie\Permission\Models\Role as SpatieRole;
+use Tenancy\Affects\Connections\Support\Traits\OnTenant;
 
 class Role extends SpatieRole
 {
     use HasFactory;
     use HasUuids;
+    use OnTenant;
 }
