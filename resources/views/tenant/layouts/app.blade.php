324200f190719b82686cd467106084fc1ecad612
Solomon Ochepa <solomonochepa@gmail.com>
2022-12-02 11:02:08 +0100

    Create Tenant AppLayout

---
+++ resources/views/tenant/layouts/app.blade.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+
+    <!-- Scripts -->
+    @vite('resources/js/app.js')
+
+    @env('production')
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    @endenv @env('local')
+    <!-- Fontawesome CSS -->
+    <link rel="stylesheet" href="http://cdn.test/font-awesome/6.1.2/css/all.min.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="http://cdn.test/bootstrap/5.0.2/css/bootstrap.min.css">
+    @endenv
+
+    <!-- Styles -->
+    @livewireStyles
+
+    @stack('css')
+
+    @stack('js.head')
+</head>
+
+<body class="font-sans antialiased">
+    {{-- <x-jet-banner /> --}}
+
+    <div class="min-h-screen bg-gray-100">
+        @livewire('tenant.navigation-menu')
+
+        <!-- Page Heading -->
+        @if (isset($header))
+            <header class="bg-white shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+        @endif
+
+        <!-- Page Content -->
+        <main>
+            {{ $slot }}
+        </main>
+    </div>
+
+    @stack('modals')
+
+    <!-- Scripts -->
+    @livewireScripts
+
+    <!-- Bootstrap -->
+    <script src="http://cdn.test/bootstrap/5.0.2/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Font-awesome -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/js/all.min.js"
+        integrity="sha512-rpLlll167T5LJHwp0waJCh3ZRf7pO6IT1+LZOhAyP6phAirwchClbTZV3iqL3BMrVxIYRbzGTpli4rfxsCK6Vw=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    @stack('js')
+</body>
+
+</html>
